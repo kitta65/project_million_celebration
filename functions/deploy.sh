36b85979fadd7fbcac152670262dfdc2f65f5510
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-07-30 11:17:24 +0900

    add ignore-file setting

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
-gcloud functions deploy million_celebration_upload --entry-point main_upload --runtime python37 --trigger-http --memory 2048MB --timeout 500s --ingress-settings internal-only --allow-unauthenticated
-gcloud functions deploy million_celebration_tweet --entry-point main_tweet --runtime python37 --trigger-http --memory 2048MB --timeout 500s --ingress-settings internal-only --allow-unauthenticated
+gcloud functions deploy million_celebration_upload --entry-point main_upload --runtime python37 --trigger-http --memory 2048MB --timeout 500s --ignore-file ./.gcloudignore --ingress-settings internal-only --allow-unauthenticated
+gcloud functions deploy million_celebration_tweet --entry-point main_tweet --runtime python37 --trigger-http --memory 2048MB --timeout 500s --ignore-file ./.gcloudignore --ingress-settings internal-only --allow-unauthenticated
